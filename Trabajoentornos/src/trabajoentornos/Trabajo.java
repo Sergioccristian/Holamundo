25951fc6974e3f864f5a8c2342fd166515f99264
pc <pc@172.16.6.12>
2018-05-10 11:38:38 +0200

    Subiendo sin actualizar

--- Trabajoentornos/src/trabajoentornos/Trabajo.java
+++ Trabajoentornos/src/trabajoentornos/Trabajo.java
@@ -8,6 +8,7 @@ public static void main(String[]args) {
 	 System.out.println("Cristian y Sergio, segundo commit");
 	 System.out.println("Primera subida para generar conflicto");
 	
+	 System.out.println("Hago un cambio sin actualizar");
  }
 }
  
